83134065f3c6c38f4995474681681cb4fedc7471
Helene BRULIN <hbrulin@e2r7p10.42.fr>
2020-01-22 16:50:01 +0100

    ft_read

--- main.c
+++ main.c
@@ -1,25 +1,36 @@
 #include <stdio.h> //mettre bon header pour size_t
+#include <fcntl.h>
+#include <unistd.h>
 
 size_t ft_strlen(const char *s);
 char *ft_strcpy(char * dst, const char * src);
 int ft_strcmp(const char *s1, const char *s2);
 ssize_t ft_write(int fildes, const void *buf, size_t nbyte);
+ssize_t ft_read(int fildes, void *buf, size_t nbyte);
 
 int main(void)
 {
 	char *s = "bonjour";
-	int i = ft_strlen(s);
-	printf("%i\n", i);
+	int ret = ft_strlen(s);
+	printf("%i\n", ret);
 
 	char cpy[7];
 	ft_strcpy(cpy, s);
 	printf("%s\n", cpy);
 
-	int ret = ft_strcmp(cpy, s);
+	ret = ft_strcmp(cpy, s);
 	printf("%i\n", ret);
 
 	ret = ft_strcmp("bonjout", s);
 	printf("%i\n", ret);
 
 	ft_write(1, "c", 1);
+	ft_write(1, "\n", 1);
+
+	int fd = open("ft_strlen.s", O_RDONLY);
+	char buf[100];
+	ret = ft_read(fd, buf, 10);
+	printf("%i\n", ret);
+	printf("%s\n", buf);
+
 }
