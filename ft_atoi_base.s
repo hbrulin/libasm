7b82ce3b6ed2c2069e8e7ec9e550bdb7602b45f8
Helene BRULIN <hbrulin@e2r6p12.42.fr>
2020-01-24 15:56:11 +0100

    whitespace base

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -10,10 +10,10 @@ _ft_atoi_base:
 	je end ;renvoie 0 si pas de base
 
 init:
-	mov rcx, -1 ;compteur, a -1 car pour le whitespace on incremente au debut
+	mov rcx, -1 ;compteur str, a -1 car pour le whitespace on incremente au debut
+	mov r12, -1 ;compteur base
 	mov r8, 0 ;nb
 	mov r9, -1 ;neg
-;whitespace
 ;checks
 
 skip:
@@ -32,13 +32,32 @@ whitespace:
 	je skip
 	cmp BYTE [rdi + rcx], 12
 	je skip
+	jmp skip_base
+
+skip_base:
+	inc r12
+
+whitespace_base:
+	cmp BYTE [rsi + r12], 32
+	je skip_base
+	cmp BYTE [rsi + r12], 9
+	je skip_base
+	cmp BYTE [rsi + r12], 10
+	je skip_base
+	cmp BYTE [rsi + r12], 13
+	je skip_base
+	cmp BYTE [rsi + r12], 11
+	je skip_base
+	cmp BYTE [rsi + r12], 12
+	je skip_base
 	jmp len
 
 len:
 	push rdi ;je save rdi (str) car ma ft_strlen va le prendre en arg
 	mov rdi, rsi ; je mets base dans rdi pour que strlen calcule la len de la base
 	call _ft_strlen ;renvoie la len dans rax -CHECK SI STRLEN NE CHANGE PAS RCX??
-	mov r10, rax ; je mets len dans r10
+	sub rax, r12
+	mov r10, rax ; je mets len dans r10, je lui retire la valeur de r12 si whitespace
 	pop rdi ; je recup str
 
 	dec rcx ;car mon compteur dans pos va etre incrementé, donc je le baisse
@@ -55,7 +74,7 @@ check_sign:
 comp:
 	mov rbx, 0
 	mov bl, BYTE [rdi + rcx]
-	cmp bl, BYTE [rsi + 0] ;base[0]
+	cmp bl, BYTE [rsi + r12] ;base[0]
 	jb end ;si inferieur a base[0]
 	cmp bl, BYTE [rsi + r10 - 1] ;base[len - 1]
 	ja end ;si superieur a base[len -1]
@@ -70,7 +89,7 @@ calc:
 	movsx rbx, BYTE [rdi + rcx]
 	add r8, rbx
 	mov rbx, 0
-	movsx rbx, BYTE [rsi + 0]
+	movsx rbx, BYTE [rsi + r12]
 	sub r8, rbx
 	inc rcx
 	jmp comp
